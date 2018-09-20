d1725fca51ea9f64c5c54980c3764202caee9988
julianloaiza <julianloaiza@javerianacali.edu.co>
2018-09-20 18:20:55 -0500

    Version Final

--- RGBtoHex.php
+++ RGBtoHex.php
@@ -1,22 +1,24 @@
 <?php
-	// Julián Andrés Loaiza
+	// Julián Loaiza
 	function dec_hex($dec) {
 	    $hex = "";
-	    if($dec == 0){$hex = "0";}
+	    if($dec == 0){$hex = "00";}
 	 	while($dec > 0){
 	 		$int  = intVal($dec/16);
 	 		$float = ($dec/16) - $int;
-	 		$digit = (string)($float*16);
-	 		if ($digit == "10"){ $digit = "A"; }
-	 		elseif($digit == "11"){ $digit = "B"; }
-	 		elseif($digit == "12"){ $digit = "C"; }
-	 		elseif($digit == "13"){ $digit = "D"; }
-	 		elseif($digit == "14"){ $digit = "E"; }
-	 		elseif($digit == "15"){ $digit = "F"; }
-	 		$hex = $digit.$hex;
+	 		$strDigit = (string)($float*16);
+	 		if ($strDigit == "10"){ $strDigit = "A"; }
+	 		elseif($strDigit == "11"){ $strDigit = "B"; }
+	 		elseif($strDigit == "12"){ $strDigit = "C"; }
+	 		elseif($strDigit == "13"){ $strDigit = "D"; }
+	 		elseif($strDigit == "14"){ $strDigit = "E"; }
+	 		elseif($strDigit == "15"){ $strDigit = "F"; }
+	 		$hex = $strDigit.$hex;
 	 		$dec = $int;
 	 	}
-    	return $hex;	
+	 	if(strlen($hex) == 1){$hex = '0'.$hex;}
+	 	assert(strlen($hex) == 2);
+    	return $hex;
 	}
 
 	function RGB_hex($R, $G, $B){
@@ -29,13 +31,15 @@
 		return dec_hex($R).dec_hex($G).dec_hex($B);
 	} 
 
-	for ($i = 1; $i <= 10; $i++) {
-		$R = rand(-300, 300);
-		$G = rand(-300, 300);
-		$B = rand(-300, 300);
-    	echo "RGB: ".$R.", ".$G.", ".$B."<br/>";
-    	echo "Hex = ".RGB_hex($R,$G,$B)."<br/> <br/>";
+	function main(){
+		for ($i = 1; $i <= 10; $i++) {
+			$R = rand(-300, 300);
+			$G = rand(-300, 300);
+			$B = rand(-300, 300);
+	    	echo "RGB: ".$R.", ".$G.", ".$B."<br/>";
+	    	echo "Hex = #".RGB_hex($R,$G,$B)."<br/> <br/>";
+		}
 	}
-	
+	main();
 
 ?>
